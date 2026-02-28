564d0c6b4e29f6a1bf5c31566dce537cf347f86b
Jayneel <jayneelkanungo57@gmail.com>
2026-02-28 19:52:32 +0530

    pom setup

---
+++ src/main/java/com/jayneel/jvd_consumer/JvdConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.jayneel.jvd_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvdConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvdConsumerApplication.class, args);
+	}
+
+}
