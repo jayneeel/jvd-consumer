1d5629fc3b504202c188d414e2b6920dbb8bbfd0
Jayneel <jayneelkanungo57@gmail.com>
2026-03-29 14:45:46 +0530

    aop module

---
+++ src/main/java/com/jayneel/jvd_consumer/aop/LoggingAspect.java
@@ -0,0 +1,18 @@
+package com.jayneel.jvd_consumer.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Before("execute(* com.jayneel.jvd_consumer.service.JobService.*(..))")
+    public void logMethodCall(){
+        LOGGER.info("Method Called");
+    }
+}
